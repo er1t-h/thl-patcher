799304f85674fae6610b54fe675d078387514f70
er1t <er1t-github.exposure048@passmail.net>
2025-10-24 17:37:29 +0200

    Added indicatif for patcher

--- crates/patcher-cli/src/main.rs
+++ crates/patcher-cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use walkdir::WalkDir;
 use std::{fs::File, path::PathBuf};
 use xz2::{read::XzDecoder, write::XzEncoder};
 
@@ -22,19 +23,22 @@ fn main() -> anyhow::Result<()> {
     let args = Argument::parse();
     match args.subcommand {
         Command::Diff => {
+            let count = WalkDir::new(&args.new).into_iter().filter(|entry| entry.as_ref().is_ok_and(|f| f.file_type().is_file())).count();
+            let progress_bar = indicatif::ProgressBar::new(count as u64);
             thl_patcher::diff_in_tar(
                 &args.old,
                 &args.new,
                 &mut tar::Builder::new(XzEncoder::new(File::create(args.destination)?, COMPRESSION_LEVEL)),
-                |_| (),
+                |_| progress_bar.inc(1),
             )?;
         }
         Command::Patch => {
+            let progress_bar = indicatif::ProgressBar::new_spinner();
             thl_patcher::patch_from_tar(
                 &args.old,
                 &mut tar::Archive::new(XzDecoder::new(File::open(args.new)?)),
                 &args.destination,
-                |_| (),
+                |current| progress_bar.set_message(format!("patching {}", current.path.display())),
             )?;
         }
     }
